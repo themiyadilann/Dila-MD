9b774ab3d56b3d68f5dac41ce2eda548d185e509
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-19 18:19:45 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -10,6 +10,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+    if(!isOwner) return
 const {exec} = require("child_process")
 reply("restarting...")
 await sleep(1500)
