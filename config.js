ad076b535d82f7a15fc0765da382f228f6c828ee
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-19 17:44:09 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID,
+};
