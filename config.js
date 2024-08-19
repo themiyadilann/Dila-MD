ee721cb0adcef83580eaa350c002375a5ce5340a
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-19 19:19:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID, || "iEkwnQCI#RoZ2zBZ1xRbPlmSEJxTi40kQDm36ngd-H_ZEKtqvpP0",
 ALIVE_IMG: process.env.ALIVE_IMG, || "https://telegra.ph/file/d5200670d0dd45e6438da.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG, || "Hello dila alive now",
 };
