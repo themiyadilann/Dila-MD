497d36159d5224db6d61189e2600f5d381617a18
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-19 18:09:23 +0530

    Add files via upload

---
+++ plugins/restart.js
@@ -0,0 +1,21 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const {sleep} = require('../lib/functions')
+
+cmd({
+    pattern: "restart",
+    desc: "restart the bot",
+    category: "owner",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+const {exec} = require("child_process")
+reply("restarting...")
+await sleep(1500)
+exec("pm2 restart all")
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
