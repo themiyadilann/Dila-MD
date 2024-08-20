746b3f2f15f1a495d1ddca06c27f0d7f82d83772
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-21 02:58:57 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,6 +1,7 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
 const os = require("os")
+const {runtime} = require('../lib/functions')
 cmd({
     pattern: "system",
   alias: ["status","botinfo"],
