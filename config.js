8abbda00a6a6ec62657a51c7237b22072d0874a0
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-21 01:38:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/d5200670d0dd45e6438da.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello dila alive now",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
