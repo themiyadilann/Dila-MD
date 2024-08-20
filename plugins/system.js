99e84ff61e8fcb92d334fdaa3a2d2c2da32f9c06
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-21 02:52:48 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,26 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+cmd({
+    pattern: "system",
+  alias: ["status","botinfo"],
+    desc: "Check up time , ram usege and more",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let status = `*Uptime:*  ${runtime(process.uptime())}
+*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*HostName:* ${os.hostname()}
+*Owner:* ᴹᵃᵈᵉ ᴮʸ ᴹʳᴰⁱˡᵃ
+`
+return reply(`${status}`)
+
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+
+}
+})
