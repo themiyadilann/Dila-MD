c3a74552e3d4428fa52a8afd34c413473e43fea2
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-21 02:05:12 +0530

    Create ai.js

---
+++ plugins/ai.js
@@ -0,0 +1,11 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "alive",
+    desc: "Check bot online or no.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
