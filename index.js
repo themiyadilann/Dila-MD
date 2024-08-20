29ad825fff60f3782835bc5c04a4da4422c19ef2
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-21 03:19:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -166,7 +166,10 @@ mek.type === "stickerMessage"
 command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
 }});
 //============================================================================ 
-
+if(!isOwner && config.MODE === "private") return
+if(!isOwner && isGroup && config.MODE === "inbox") return
+if(!isOwner && !isGroup && config.MODE === "groups") return
+        //==============
 })
 }
 app.get("/", (req, res) => {
