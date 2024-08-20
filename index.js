e7c0a7a1c08b6128b75411845ed7013921ea95ef
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-21 01:46:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,10 @@ conn.ev.on('messages.upsert', async(mek) => {
 mek = mek.messages[0]
 if (!mek.message) return	
 mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
+await conn.readMessages([mek.key]) 
+
+}
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
 const content = JSON.stringify(mek.message)
