2282b30292a3b1da3e29e3dfd955a9f6b0c9169f
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-21 10:54:22 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -2,7 +2,7 @@ const config = require('../config')
 const {cmd , commands} = require('../command')
 
 cmd({
-    pattern: "alive",
+    pattern: "dilo",
     desc: "Check bot online or no.",
     category: "main",
     filename: __filename
