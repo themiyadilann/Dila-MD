6d697064edf389de48680352a738c95d087d8293
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-23 03:59:01 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,27 +1,24 @@
 const config = require('../config')
-const {cmd , commands} = require('../command')
+const {cmd, commands} = require('../command')
 const os = require("os")
 const {runtime} = require('../lib/functions')
+
 cmd({
     pattern: "system",
-  alias: ["status","botinfo"],
-    desc: "Check up time , ram usege and more",
+    alias: ["status", "botinfo"],
+    desc: "Check uptime, RAM usage, and more",
     category: "main",
     filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-let status = `*Uptime:*  ${runtime(process.uptime())}
-*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-*HostName:* ${os.hostname()}
-*Owner:* ᴹᵃᵈᵉ ᴮʸ ᴹʳᴰⁱˡᵃ
+}, async (conn, mek, m, {from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+    try {
+        let status = `*🕒 Uptime:*  ${runtime(process.uptime())}
+*💾 RAM Usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem() / 1024 / 1024)}MB
+*🏠 HostName:* ${os.hostname()}
+*👤 Owner:* ᴹᵃᵈᵉ ᴮʸ ᴹʳᴰⁱˡᵃ
 `
-return reply(`${status}`)
-
-
-}catch(e){
-console.log(e)
-reply(`${e}`)
-
-}
+        return reply(`${status}`)
+    } catch (e) {
+        console.log(e)
+        reply(`Error: ${e}`)
+    }
 })
