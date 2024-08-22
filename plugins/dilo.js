ab6bfb3c76d45ce275f3db87468df01bd0429bfa
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-23 03:00:41 +0530

    Update dilo.js

--- plugins/dilo.js
+++ plugins/dilo.js
@@ -1,16 +1,16 @@
-const config = require('../config')
-const {cmd , commands} = require('../command')
+const config = require('../config');
+const { cmd } = require('../command');
+
 cmd({
-    pattern: "dilo",
+    pattern: "dilo(.*)", // Regular expression to match multiple patterns
     desc: "Check bot online or no.",
     category: "main",
     filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-return await conn.sendMessage(from,{image: {url: config.DILO_IMG},caption: config.DILO_MSG},{quoted: mek})
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+}, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        return await conn.sendMessage(from, { image: { url: config.DILO_IMG }, caption: config.DILO_MSG }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
