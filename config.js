f400b5cfcdbd11db7a298230e29dfc42df39ba08
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-22 23:41:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "GJNSHLwb#0IK-xVxp95Wd5paflE99799kNeTc78uvqY-JSW1vMBA",
+SESSION_ID: process.env.SESSION_ID || "zBcSGJLD#YUTF8LQz7fwEopPGnhJTYkt37GuVOXqS2NxLwDb-IV0",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/dcd097f9f7a124d47e5b2.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*𝗜'𝗺 𝗔𝗹𝗶𝘃𝗲 𝗡𝗼𝘄 ♥*\n*𝚃𝚛𝚢 𝚃𝚑𝚒𝚜 ⤵*\n\n.ai (Your question)\n_example - .ai Hey_\n\n*Support Us ⤵*\n𝚆𝚑𝚊𝚝𝚜𝚊𝚙𝚙 - https://whatsapp.com/channel/0029ValK0gn4SpkP6iaXoj2y\n𝚈𝚘𝚞𝚝𝚞𝚋𝚎 - https://youtube.com/@dila_lk\n𝚆𝚎𝚋 𝚂𝚒𝚝𝚎 - dilalk.vercel.app\n\nᴍᴀᴅᴇ ʙʏ ᴍʀᴅɪʟᴀ",
 DILO_IMG: process.env.DILO_IMG || "https://telegra.ph/file/dcd097f9f7a124d47e5b2.jpg",
