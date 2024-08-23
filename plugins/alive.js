68782f557ecc5d11785882cfe54a9e8c618ef816
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-23 08:34:05 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -8,12 +8,14 @@ cmd({
     filename: __filename
 }, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
-        // Send image with caption
-        await conn.sendMessage(from, { image: { url: config.ALIVE_IMG }, caption: config.ALIVE_MSG }, { quoted: mek });
+        // Only proceed if the message is not a command
+        if (!isCmd) {
+            // Send image with caption
+            await conn.sendMessage(from, { image: { url: config.ALIVE_IMG }, caption: config.ALIVE_MSG }, { quoted: mek });
 
-        // Send audio as a voice message
-        await conn.sendMessage(from, { audio: { url: 'https://drive.google.com/uc?export=download&id=1UtbyKhdUoVWTJlwSGRUFo6tl3dAq42mF' }, mimetype: 'audio/mp4' }, { quoted: mek });
-        
+            // Send audio as a voice message
+            await conn.sendMessage(from, { audio: { url: 'https://drive.google.com/uc?export=download&id=1UtbyKhdUoVWTJlwSGRUFo6tl3dAq42mF' }, mimetype: 'audio/mp4' }, { quoted: mek });
+        }
     } catch (e) {
         console.log(e);
         reply(`Error: ${e.message}`);
