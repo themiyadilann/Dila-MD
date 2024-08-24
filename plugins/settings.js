ba261215951075464e1e6f1fe486cd32c1bd29b5
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-24 12:18:48 +0530

    Update settings.js

--- plugins/settings.js
+++ plugins/settings.js
@@ -11,7 +11,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
     try {
         return await conn.sendMessage(from, {
             image: { url: 'https://telegra.ph/file/959a00b8f92106a8856de.jpg' },
-            caption: 'hi, setting now karanna'
+            caption: '𝗗𝗜𝗟𝗔𝗠𝗗 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦 ⚙️\n\n*Cheng Alive&Menu IMG*\n.update ALIVE_IMG: Your Image Url\n\n*Cheng Alive MSG*\n.update ALIVE_IMG: Your Alive MSG\n\n*Cheng Prefix*\n.update PERFIX: your prefix (.,$#%&)\n\n*Auto Status Seen*\n.update AUTO_READ_STATUS: true or false\n\n*Mode*\n.update MODE: private, public, index or group'
         }, { quoted: mek });
     } catch (e) {
         console.log(e);
