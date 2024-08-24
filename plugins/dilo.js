ea395a000f490eb7a7a1611b53462242f185c2c6
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-24 12:31:51 +0530

    Update dilo.js

--- plugins/dilo.js
+++ plugins/dilo.js
@@ -1,16 +1,19 @@
-const config = require('../config')
-const {cmd , commands} = require('../command')
+const { cmd, commands } = require('../command');
+
 cmd({
     pattern: "dilo",
-    desc: "Check bot online or no.",
+    desc: "Check bot online or not.",
     category: "main",
     filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-return await conn.sendMessage(from,{image: {url: config.DILO_IMG},caption: config.DILO_MSG},{quoted: mek})
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+}, 
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        return await conn.sendMessage(from, {
+            image: { url: 'https://telegra.ph/file/dcd097f9f7a124d47e5b2.jpg' },
+            caption: '*Name*: Dilan\n*From*: Matara\n*Age*: 20\n*web* : dilalk.vercel.app\n\n_you .....?_ 🤖'
+        }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
