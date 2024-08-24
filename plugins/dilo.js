0d23c6ff6016baf2c2e2c4a95291b79986c159d0
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-24 10:11:09 +0530

    Update dilo.js

--- plugins/dilo.js
+++ plugins/dilo.js
@@ -1,4 +1,4 @@
-const config = require('../config')
+const {readEnv} = require('../database')
 const {cmd , commands} = require('../command')
 cmd({
     pattern: "dilo",
@@ -8,6 +8,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+const config await readEnv();
 return await conn.sendMessage(from,{image: {url: config.DILO_IMG},caption: config.DILO_MSG},{quoted: mek})
 }catch(e){
 console.log(e)
