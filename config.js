b547a18008cf473e489ba084f78460dfe60645f4
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-24 14:17:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "zBcSGJLD#YUTF8LQz7fwEopPGnhJTYkt37GuVOXqS2NxLwDb-IV0",
-MONGODB: process.env.MONGODB || "mongodb+srv://wociha9652:<db_L4icJVS19hf5djtk>@cluster0.la4bi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+MONGODB: process.env.MONGODB || "mongodb://${{MONGO_INITDB_ROOT_USERNAME}}:${{MONGO_INITDB_ROOT_PASSWORD}}@${{RAILWAY_TCP_PROXY_DOMAIN}}:${{RAILWAY_TCP_PROXY_PORT}}",
 };
