53ca6273d00a806339abe82136d9ee68f0b34601
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-24 16:26:24 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -11,32 +11,20 @@ cmd({
     filename: __filename
 }, async (conn, mek, m, {from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
     try {
-        const start = Date.now(); // Start time for ping calculation
-
-        // Reply to measure the ping
-        const sentMsg = await reply('Calculating ping...');
-
-        const end = Date.now(); // End time after message is sent
-        const ping = end - start; // Calculate ping
-
         // RAM usage
         const totalRAM = Math.round(require('os').totalmem() / 1024 / 1024); // Total RAM in MB
         const usedRAM = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2); // Used RAM in MB
         const freeRAM = (totalRAM - parseFloat(usedRAM)).toFixed(2); // Free RAM in MB
 
-        const status = `*🕒 Uptime:* ${runtime(process.uptime())}
+        let status = `*🕒 Uptime:*  ${runtime(process.uptime())}
 *💾 RAM Usage:* 
 - *Used*: ${usedRAM} MB
 - *Free*: ${freeRAM} MB
 - *Total*: ${totalRAM} MB
 *🏠 HostName:* ${os.hostname()}
 *👤 Owner:* ᴹᵃᵈᵉ ᴮʸ ᴹʳᴰⁱˡᵃ
-*📶 Ping:* ${ping} ms
 `
-
-        // Update the message with the status and ping
-        await conn.sendMessage(from, { text: status }, { quoted: sentMsg });
-        
+        return reply(`${status}`)
     } catch (e) {
         console.log(e)
         reply(`Error: ${e}`)
