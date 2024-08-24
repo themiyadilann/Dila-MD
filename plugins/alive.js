d5407bd5d3bb212a97d6ab11dc2883beea6cedc3
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-24 13:26:05 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,4 +1,4 @@
-const config = require('../config');
+const {readEnv} = require('../lib/database')
 const { cmd } = require('../command');
 
 cmd({
@@ -8,6 +8,8 @@ cmd({
     filename: __filename
 }, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
+
+    const config = await readEnv();
         // Send image with caption
         await conn.sendMessage(from, { image: { url: config.ALIVE_IMG }, caption: config.ALIVE_MSG }, { quoted: mek });
 
