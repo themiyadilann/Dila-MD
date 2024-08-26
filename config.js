c3537a393dcbcffd0c261129a5efa6568b5c291b
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-26 08:56:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ydcwjCIC#toQ8ea-3MK--7V5WsbZf97yib9zi_7RN1ldssymoVn4",
-DILO_VIDEO: process.env.DILO_IMG || "https://drive.google.com/uc?export=download&id=1YUJHTnjKhbA-m1PYRyAhC4K7u1PZvKYC",
+DILO_IMG: process.env.DILO_IMG || "https://telegra.ph/file/dcd097f9f7a124d47e5b2.jpg",
 DILO_MSG: process.env.DILO_MSG || "*Name*: Dilan\n*From*: Matara\n*Age*: 20\n*web* : dilalk.vercel.app\n\n_you .....?_ 🤖",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
