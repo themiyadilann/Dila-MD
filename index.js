08729ffbf8c3a51e790a4c87a681560b8a3665fd
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-28 10:44:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -188,6 +188,71 @@ command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, i
 
 })
 }
+
+
+
+
+
+
+
+
+
+const { sms, downloadMediaMessage } = require('./lib/msg'); // Import from msg.js
+const events = require('./lib/msg'); // Import the updated command management functions
+
+const cmdName = isCmd ? body.slice(1).trim().split(" ")[0].toLowerCase() : false;
+if (isCmd) {
+    const cmd = events.commands.find((cmd) => cmd.pattern === cmdName) || events.commands.find((cmd) => cmd.alias && cmd.alias.includes(cmdName));
+    
+    if (cmd) {
+        if (cmd.react) conn.sendMessage(from, { react: { text: cmd.react, key: mek.key }});
+
+        try {
+            cmd.function(conn, mek, m, {
+                from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, 
+                botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, 
+                participants, groupAdmins, isBotAdmins, isAdmins, reply 
+            });
+        } catch (e) {
+            console.error("[PLUGIN ERROR] " + e);
+        }
+    }
+}
+
+events.commands.map(async (command) => {
+    if (body && command.on === "body") {
+        command.function(conn, mek, m, {
+            from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, 
+            botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, 
+            participants, groupAdmins, isBotAdmins, isAdmins, reply
+        });
+    } else if (mek.q && command.on === "text") {
+        command.function(conn, mek, m, {
+            from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, 
+            botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, 
+            participants, groupAdmins, isBotAdmins, isAdmins, reply
+        });
+    } else if ((command.on === "image" || command.on === "photo") && mek.type === "imageMessage") {
+        command.function(conn, mek, m, {
+            from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, 
+            botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, 
+            participants, groupAdmins, isBotAdmins, isAdmins, reply
+        });
+    } else if (command.on === "sticker" && mek.type === "stickerMessage") {
+        command.function(conn, mek, m, {
+            from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, 
+            botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, 
+            participants, groupAdmins, isBotAdmins, isAdmins, reply
+        });
+    }
+});
+
+
+
+
+
+
+
 app.get("/", (req, res) => {
 res.send("hey, bot started✅");
 });
