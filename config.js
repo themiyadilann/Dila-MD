cb9c119bf3c1d2c1e5d89886cfeb62e3dd7d5724
themiyadilan <116650623+themiyadilan@users.noreply.github.com>
2024-08-29 15:59:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "aN8GzJRR#TlAddL1CUl8F376x1kU3NeOSyWWBNbxc5rZxpywd2oY",
+SESSION_ID: process.env.SESSION_ID || "Z6sGkRSb#NB5J0orOv_TnW8JlYxhpexQd7918iNt3CeXgdxNO3uw",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 };
