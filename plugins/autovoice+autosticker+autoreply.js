4c8bb3e15a1d38e64e16123bcb1a0ac22cd81d92
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-30 23:30:37 +0530

    Update autovoice+autosticker+autoreply.js

--- plugins/autovoice+autosticker+autoreply.js
+++ plugins/autovoice+autosticker+autoreply.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const {readEnv} = require('../lib/database')
+const {config} = require('./config')
 const {cmd , commands} = require('../command')
 
 //auto_voice
@@ -12,7 +12,7 @@ async (conn, mek, m, { from, body, isOwner }) => {
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
+            const config = await config();
             if (config.AUTO_VOICE === 'true') {
                 //if (isOwner) return;        
                 await conn.sendPresenceUpdate('recording', from);
@@ -31,7 +31,7 @@ async (conn, mek, m, { from, body, isOwner }) => {
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
+            const config = await config();
             if (config.AUTO_STICKER === 'true') {
                 //if (isOwner) return;        
                 await conn.sendMessage(from,{sticker: { url : data[text]},package: 'yourName'},{ quoted: mek })   
@@ -50,7 +50,7 @@ async (conn, mek, m, { from, body, isOwner }) => {
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
+            const config = await config();
             if (config.AUTO_REPLY === 'true') {
                 //if (isOwner) return;        
                 await m.reply(data[text])
