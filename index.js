4416e1e06ec12d564f0c55087089366e358615c0
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-31 21:03:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,10 @@ const connectDB = require('./lib/mongodb')
 connectDB();
 // ===================
 const {readEnv} = require('./lib/database')
-
+const config = await readEnv();
+const prefix = config.PREFIX
+//=====================
+        
 console.log("Connecting wa bot 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
