e4eb7e7e4ba4fc5faf2942913e1b0b7b98d2560b
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-31 21:07:56 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,4 +1,4 @@
-const config = require('../config')
+const {readEnv} = require('../lib/database')
 const {cmd, commands} = require('../command')
 const os = require("os")
 const {runtime} = require('../lib/functions')
