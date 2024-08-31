51b285f2376694b7ae13a89489c4aefe6218495c
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-08-31 20:51:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,9 @@ const port = process.env.PORT || 8000;
 //=============================================
 
 async function connectToWA() {
+
+// connect mongodb 
+const connectDB = require(./lib/mongodb)
 console.log("Connecting wa bot 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
