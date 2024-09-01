9206a008cba92f4feca27c32522ad41d7a829711
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-09-01 19:41:48 +0530

    Update update_env.js

--- plugins/update_env.js
+++ plugins/update_env.js
@@ -13,7 +13,7 @@ async (conn, mek, m, { from, q, reply, isOwner }) => {
     if (!isOwner) return;
 
     if (!q) {
-        return reply("🙇‍♂️ *Please provide the environment variable and its new value.* \n\nExample: `.update ALIVE_MSG: hello i am prabath kumara`");
+        return reply("🙇‍♂️ *Please provide the environment variable and its new value.* \n\nExample: `.update ALIVE_MSG: hello i am Themiya Dilan`");
     }
 
     // Find the position of the first colon or comma
