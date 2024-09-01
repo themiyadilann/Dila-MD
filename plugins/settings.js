93ab3f0d88ffc0a3b16fabf51424202019428b4c
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-09-01 21:02:39 +0530

    Update settings.js

--- plugins/settings.js
+++ plugins/settings.js
@@ -11,7 +11,8 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
     try {
         return await conn.sendMessage(from, {
             image: { url: 'https://telegra.ph/file/959a00b8f92106a8856de.jpg' },
-            caption: '𝗗𝗜𝗟𝗔𝗠𝗗 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦 ⚙️\n\n*Cheng Alive&Menu IMG*\n.update ALIVE_IMG: Your Image Url\n\n*Cheng Alive MSG*\n.update ALIVE_MSG: Your Alive MSG\n\n*Cheng Prefix*\n.update PERFIX: your prefix (.,$#%&)\n\n*Auto Status Seen*\n.update AUTO_READ_STATUS: true or false\n\n*Mode*\n.update MODE: private, public, index or group'
+            caption: '𝗗𝗜𝗟𝗔𝗠𝗗 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦 ⚙️\n\n*Cheng Alive&Menu IMG*\n.update ALIVE_IMG: Your Image Url\n\n*Cheng Alive MSG*\n.update ALIVE_MSG: Your Alive MSG\n\n*Cheng Prefix*\n.update PERFIX: your prefix (.,$#%&)\n\n*Auto Status Seen*\n.update AUTO_READ_STATUS: true or false\n\n*Mode*\n.update MODE: private, public, index or group\n\n*Auto Reply*\n.update AUTO_VOICE: true or false\n.update AUTO_IMAGE: true or false\n.update AUTO_VIDEO: true or false\n.update AUTO_STICKER: true or false\n.update AUTO_REPLY: true or false'
+                
         }, { quoted: mek });
     } catch (e) {
         console.log(e);
