a00f54fc979cec2e6cf4110e76c0ed9bd26c37e0
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-09-01 18:04:12 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,22 +11,17 @@ cmd({
 }, async (conn, mek, m, {from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
     try {
     const config = await readEnv();
-        // RAM usage
-        const totalRAM = Math.round(require('os').totalmem() / 1024 / 1024); // Total RAM in MB
-        const usedRAM = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2); // Used RAM in MB
-        const freeRAM = (totalRAM - parseFloat(usedRAM)).toFixed(2); // Free RAM in MB
-
-
-
+        
         // Send the image with the status as the caption
         await conn.sendMessage(from, {
             image: { url: config.ALIVE_IMG },
             caption: config.ALIVE_MSG
         }, { quoted: mek || null });
         
-   await conn.sendMessage(from, { audio: { url: 'https://drive.google.com/uc?export=download&id=1XZ6bF15m4V3VPXMdt0d2gBTJwLh8Xby7' }, mimetype: 'audio/ogg; codecs=opus', ptt: true }, { quoted: mek });
+   await conn.sendPresenceUpdate('recording', from);
+   await conn.sendMessage(from, { audio: '../media/voice/alive.mp3' , mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
 
- } catch (e) {
+    } catch (e) {
         console.log(e)
         reply(`Error: ${e}`)
     }
