f82cee603ef8eaf196ba1af17038175a30493cb7
Chathuragangulel <chathuragangulel@gmail.com>
2024-09-09 09:19:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "6VNEjSxS#JZB-cSysc5mpLHiFnkcd9N5tVuNmLAM3occwuLCS0ME",
 MONGODB: process.env.MONGODB || "",
 };
