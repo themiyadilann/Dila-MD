ea1d5472eb86562d1d2b82d615b1e3c1e37904a6
themiyadilan <116650623+themiyadilan@users.noreply.github.com>
2024-09-16 17:33:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",
+MONGODB: process.env.MONGODB || "mongodb://mongo:ImGjDLgPunMYcZGAqXoZuExgneIhoiiC@autorack.proxy.rlwy.net:44968",
 };
