5a23ad1514ba70d31038e6ad486f3832ed7556b8
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-09-22 22:15:46 +0530

    Update goctime.js

--- plugins/goctime.js
+++ plugins/goctime.js
@@ -1,5 +1,6 @@
 const { cmd } = require('../command');
-const schedule = require('node-schedule'); // Make sure to install this package
+const schedule = require('node-schedule');
+const moment = require('moment-timezone'); // Make sure to install this package
 
 let openTime, closeTime;
 
@@ -11,12 +12,15 @@ cmd({ pattern: "opentime", desc: "Set daily open time for the group", category: 
     if (args.length < 1) return reply('Usage: .opentime <HH:mm>');
 
     openTime = args[0];
-    schedule.scheduleJob('openGroup', `0 ${openTime.split(':')[1]} ${openTime.split(':')[0]} * * *`, async () => {
+    const [hour, minute] = openTime.split(':').map(Number);
+    const openCron = `0 ${minute} ${hour} * * *`;
+
+    schedule.scheduleJob('openGroup', openCron, async () => {
         await conn.groupSettingUpdate(from, 'not_announcement');
-        await conn.sendMessage(from, { text: `Group opened at ${openTime}. 🔓\nᴍʀ ᴅɪʟᴀ ᴏꜰᴄ` });
+        await conn.sendMessage(from, { text: `Group opened at ${openTime} (Asia/Colombo time). 🔓\nᴍʀ ᴅɪʟᴀ ᴏꜰᴄ` });
     });
 
-    reply(`Group will open daily at ${openTime}. ⏰`);
+    reply(`Group will open daily at ${openTime} (Asia/Colombo time). ⏰`);
 });
 
 cmd({ pattern: "closetime", desc: "Set daily close time for the group", category: "group", filename: __filename }, async (conn, mek, m, { from, args, isGroup, isBotAdmins, isAdmins, reply }) => {
@@ -27,10 +31,13 @@ cmd({ pattern: "closetime", desc: "Set daily close time for the group", category
     if (args.length < 1) return reply('Usage: .closetime <HH:mm>');
 
     closeTime = args[0];
-    schedule.scheduleJob('closeGroup', `0 ${closeTime.split(':')[1]} ${closeTime.split(':')[0]} * * *`, async () => {
+    const [hour, minute] = closeTime.split(':').map(Number);
+    const closeCron = `0 ${minute} ${hour} * * *`;
+
+    schedule.scheduleJob('closeGroup', closeCron, async () => {
         await conn.groupSettingUpdate(from, 'announcement');
-        await conn.sendMessage(from, { text: `Group closed at ${closeTime}. 🔒\nᴍʀ ᴅɪʟᴀ ᴏꜰᴄ` });
+        await conn.sendMessage(from, { text: `Group closed at ${closeTime} (Asia/Colombo time). 🔒\nᴍʀ ᴅɪʟᴀ ᴏꜰᴄ` });
     });
 
-    reply(`Group will close daily at ${closeTime}. ⏰`);
+    reply(`Group will close daily at ${closeTime} (Asia/Colombo time). ⏰`);
 });
