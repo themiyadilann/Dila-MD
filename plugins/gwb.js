278f9b0d7375c331bf4ab4febc6799294ee2dd73
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-09-24 01:12:48 +0530

    Update gwb.js

--- plugins/gwb.js
+++ plugins/gwb.js
@@ -1,18 +1,20 @@
-const { cmd } = require('../command');
-const sensitiveData = require('../dila_md_licence/a/b/c/d/dddamsbs');
+module.exports = (conn) => {
+    const { cmd } = require('../command');
+    const sensitiveData = require('../dila_md_licence/a/b/c/d/dddamsbs');
 
-// Function to send welcome message to new members
-const sendWelcomeMessage = async (conn, groupId, memberId) => {
-    const welcomeMessage = `*Welcome to the group, @${memberId.split('@')[0]}! 🎉*\n*Feel free to introduce yourself and have fun!*\n${sensitiveData.footerText}`;
-    await conn.sendMessage(groupId, { text: welcomeMessage, mentions: [memberId] });
+    // Function to send welcome message to new members
+    const sendWelcomeMessage = async (groupId, memberId) => {
+        const welcomeMessage = `*Welcome to the group, @${memberId.split('@')[0]}! 🎉*\n*Feel free to introduce yourself and have fun!*\n${sensitiveData.footerText}`;
+        await conn.sendMessage(groupId, { text: welcomeMessage, mentions: [memberId] });
+    };
+
+    // Event listener for new group participants
+    conn.ev.on('group-participants.update', async (update) => {
+        const { id, participants, action } = update; // id = group id, participants = new members, action = add/remove
+        if (action === 'add') {  // Check if the action is a new member joining
+            participants.forEach(async (participant) => {
+                await sendWelcomeMessage(id, participant);  // Send welcome message to each new member
+            });
+        }
+    });
 };
-
-// Event listener for new group participants
-conn.ev.on('group-participants.update', async (update) => {
-    const { id, participants, action } = update; // id = group id, participants = new members, action = add/remove
-    if (action === 'add') {  // Check if the action is a new member joining
-        participants.forEach(async (participant) => {
-            await sendWelcomeMessage(conn, id, participant);  // Send welcome message to each new member
-        });
-    }
-});
