1f101bbe7eb1b7269a5715b5b41651487b3266ae
Tcroneb Hackx <cartoonboyzfamily@gmail.com>
2024-10-03 14:18:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "IUIXCIyS#Of73jERIoNwlq3ZGA5NVzg88bkQAQk2mclME9WNAnTM",
+SESSION_ID: process.env.SESSION_ID || " ",
 MONGODB: process.env.MONGODB || "mongodb://${{MONGO_INITDB_ROOT_USERNAME}}:${{MONGO_INITDB_ROOT_PASSWORD}}@${{RAILWAY_TCP_PROXY_DOMAIN}}:${{RAILWAY_TCP_PROXY_PORT}}",
 };
